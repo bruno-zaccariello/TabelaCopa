13f8a0f33b677360f71d28e056e8ab9f0359a983
Bruno Zaccariello <30813635+SpaceTheArcher@users.noreply.github.com>
2018-06-18 10:20:13 -0300

    Update settings.py

--- tabelacopa/settings.py
+++ tabelacopa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9l^%9v1&euj=!p62z25k+a6!dr2h8ctu-)=+!^(u9hdn75&ak#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tabelacopa.azurewebsites.net']
 
