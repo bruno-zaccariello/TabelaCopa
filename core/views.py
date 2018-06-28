20325ef99f2d575b618f2e300404356fb34a5001
Bruno Zaccariello <30813635+SpaceTheArcher@users.noreply.github.com>
2018-06-28 19:35:29 -0300

    View Fix

--- core/views.py
+++ core/views.py
@@ -90,12 +90,7 @@ def cadastro(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('')
+            return HttpResponseRedirect('login')
     else:
         form = UserCreationForm()
     context = {
